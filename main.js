89cdd5e8a1b04b78db0cd67a5822b1a0f1133d33
TirthM89 <98802520+TirthM89@users.noreply.github.com>
2023-03-09 17:22:32 +1030

    Add files via upload

---
+++ main.js
@@ -0,0 +1,54 @@
+var img = "";
+status = "";
+objects = [];
+
+function preload() {
+    img = loadImage("dog_cat.jpg");
+}
+
+function setup() {
+    canvas = createCanvas(380, 380);
+    canvas.center();
+    video = createCapture(VIDEO);
+    video.size(380,380);
+    video.hide();
+}
+
+function start() {
+    objectDetector = ml5.objectDetector("cocossd", modelLoaded);
+    document.getElementById("status").innerHTML = "Status: Detecting Objects" ;    
+}
+
+function modelLoaded() {
+    console.log("model is loaded");
+    status = true;
+    objectDetector.detect(video, gotResult);
+}
+
+function gotResult(error, results) {
+    if (error) {
+        console.error(error);
+    }
+        console.log(results);
+        objects = results;
+
+}
+
+function draw() {
+    image(video,0,0,380,380);
+    if (status != "") {
+        r = random(255);
+        g = random(255);
+        b = random(255);
+        for (let i = 0; i < objects.length; i++) {
+            document.getElementById("status").innerHTML = "Status: Object Detected";
+            fill(r,g,b);
+            percent = Math.floor(objects[i].confidence*100);
+            text(objects[i].label+" "+percent +"%",objects[i].x+4,objects[i].y+12);
+            noFill();
+            stroke(r,g,b);
+            rect(objects[i].x,objects[i].y,objects[i].width,objects[i].height);           
+        }
+    }
+}
+
